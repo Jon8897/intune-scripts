e2b06bbc7ddf558cdec2f44f4216633859bf9d90
jon8897 <jontykeefe8@gmail.com>
2023-04-28 16:17:46 +0100

    Creating log script

--- WindowsUpdateCheck.ps1
+++ WindowsUpdateCheck.ps1
@@ -1,4 +1,14 @@
-# Checks Windows update errors and logs them 
-$updates = Get-WindowsUpdate -InstallationResult 'Failed', 'SucceededWithErrors'
-$logfile = "$env:temp\WindowsUpdate.log"
-$updates | Format-Table -AutoSize | Out-File -FilePath $logfile -Encoding UTF8
+# Define the installation results that we want to check for (Failed, SucceededWithErrors)
+$installationResults = 'Failed', 'SucceededWithErrors'
+
+# Get a list of Windows updates with the specified installation results
+$updates = Get-WindowsUpdate -InstallationResult $installationResults
+
+# Define the path to the log file
+$logFile = "$env:temp\WindowsUpdate.log"
+
+# Format the list of updates as a table and save it to the log file
+$updates | Format-Table -AutoSize | Out-File -FilePath $logFile -Encoding UTF8
+
+# Display a message indicating that the script has completed
+Write-Host "Windows update check completed. Results saved to $logFile."
