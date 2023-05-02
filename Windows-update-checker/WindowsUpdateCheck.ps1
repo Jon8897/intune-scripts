180139da50a66358e4bb7f1daf0728a6c6e4c9ae
jon8897 <jontykeefe8@gmail.com>
2023-05-02 16:55:11 +0100

    Fixing script

--- Windows-update-checker/WindowsUpdateCheck.ps1
+++ Windows-update-checker/WindowsUpdateCheck.ps1
@@ -1,14 +1,15 @@
-# Define the installation results that we want to check for (Failed, SucceededWithErrors)
+# Check for Windows updates with the specified installation results
 $installationResults = 'Failed', 'SucceededWithErrors'
+$updates = Get-WUList -WindowsUpdate
 
-# Get a list of Windows updates with the specified installation results
-$updates = Get-WindowsUpdate -InstallationResult $installationResults
+# Filter the list of updates based on the installation results
+$updates = $updates | Where-Object { $installationResults -contains $_.Result }
 
 # Define the path to the log file
-$logFile = "$env:temp\WindowsUpdate.log"
+$logFile = "$env:Desktop\WindowsUpdate.log"
 
 # Format the list of updates as a table and save it to the log file
 $updates | Format-Table -AutoSize | Out-File -FilePath $logFile -Encoding UTF8
 
 # Upload the log file to Intune
-Set-Content -Path "C:\Users\jonathankeefe.ASTONBERKELEY\OneDrive - Aston Berkeley Systems Ltd\Desktop\Projects\git-projects\intune-scripts\Windows-update-checker\WindowsUpdate.log" -Value (Get-Content $logFile -Raw) -Encoding UTF8
+Set-Content -Path "$env:USERPROFILE\Desktop\WindowsUpdate.log" -Value (Get-Content $logFile -Raw) -Encoding UTF8
