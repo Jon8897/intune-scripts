425b901cca5acbb2f22974e0e36037dd25fd60ac
jon8897 <jontykeefe8@gmail.com>
2023-05-03 13:46:29 +0100

    Added loading bar amd display message

--- Windows-update-checker/WindowsUpdateCheck.ps1
+++ Windows-update-checker/WindowsUpdateCheck.ps1
@@ -9,15 +9,27 @@ $updates = $updates | Where-Object { $installationResults -contains $_.Result }
 $logFile = "$env:Desktop\WindowsUpdate.log"
 
 if ($updates) {
+    # Create a loading bar
+    Write-Progress -Activity "Checking for Windows updates..." -Status "Please wait..." -PercentComplete 0
+
     # Format the list of updates as a table and save it to the log file
     $updates | Format-Table -AutoSize | Out-File -FilePath $logFile -Encoding UTF8
 
     # Upload the log file to Intune
     Set-Content -Path "$env:USERPROFILE\Desktop\WindowsUpdate.log" -Value (Get-Content $logFile -Raw) -Encoding UTF8
+
+    # Update the loading bar
+    Write-Progress -Activity "Checking for Windows updates..." -Status "Completed" -PercentComplete 100 -Completed
+
+    # Display success message
+    Write-Host "Windows updates checked successfully. Please check the log file for details."
 } else {
     # Write "Update not needed" to the log file
     "Update not needed" | Out-File -FilePath $logFile -Encoding UTF8
 
     # Upload the log file to Intune
     Set-Content -Path "$env:USERPROFILE\Desktop\WindowsUpdate.log" -Value "Update not needed" -Encoding UTF8
+
+    # Display success message
+    Write-Host "No Windows updates needed. Please check the log file for details."
 }
