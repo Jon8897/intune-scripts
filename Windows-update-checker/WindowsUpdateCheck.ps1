3d1dd1faa49eeac672b8141867cedf23f0eccf6b
jon8897 <jontykeefe8@gmail.com>
2023-05-03 12:30:36 +0100

    Adding if update not needed message

--- Windows-update-checker/WindowsUpdateCheck.ps1
+++ Windows-update-checker/WindowsUpdateCheck.ps1
@@ -8,6 +8,20 @@ $updates = $updates | Where-Object { $installationResults -contains $_.Result }
 # Define the path to the log file
 $logFile = "$env:Desktop\WindowsUpdate.log"
 
+if ($updates) {
+    # Format the list of updates as a table and save it to the log file
+    $updates | Format-Table -AutoSize | Out-File -FilePath $logFile -Encoding UTF8
+
+    # Upload the log file to Intune
+    Set-Content -Path "$env:USERPROFILE\Desktop\WindowsUpdate.log" -Value (Get-Content $logFile -Raw) -Encoding UTF8
+} else {
+    # Write "Update not needed" to the log file
+    "Update not needed" | Out-File -FilePath $logFile -Encoding UTF8
+
+    # Upload the log file to Intune
+    Set-Content -Path "$env:USERPROFILE\Desktop\WindowsUpdate.log" -Value "Update not needed" -Encoding UTF8
+}
+
 # Format the list of updates as a table and save it to the log file
 $updates | Format-Table -AutoSize | Out-File -FilePath $logFile -Encoding UTF8
 
