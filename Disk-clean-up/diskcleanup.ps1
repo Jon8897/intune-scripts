b8c17628c2f317874eb94d0a33b23b2c1fd96aae
jon8897 <jontykeefe8@gmail.com>
2023-05-22 12:05:18 +0100

    Diskcleanup added

---
+++ Disk-clean-up/diskcleanup.ps1
@@ -0,0 +1,10 @@
+# Set up the cleanup flags
+$CleanupFlags = @{
+    "DownloadedProgramFiles" = $True
+    "TemporaryInternetFiles" = $True
+    "Thumbnails" = $True
+    "RecycleBin" = $True
+    "TemporaryFiles" = $True
+    "DeliveryOptimizationFiles" = $True
+    "SetupLogFiles" = $True
+}
