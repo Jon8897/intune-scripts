e4a0ac666f38ba6411468dccf264157081889a52
jon8897 <jontykeefe8@gmail.com>
2023-05-22 12:08:48 +0100

    DISM session and component

--- Disk-clean-up/diskcleanup.ps1
+++ Disk-clean-up/diskcleanup.ps1
@@ -12,3 +12,12 @@ $CleanupFlags = @{
 # Display a popup message to the user indicating that the cleanup is in progress
 $CleanupOptions = New-Object -ComObject "WScript.Shell"
 $CleanupOptions.Popup("Performing Disk Cleanup. Please wait...", 0, "Disk Cleanup", 64)
+
+# Create a DISM session and component, and configure the log file location and verbosity
+$CleanupManager = New-Object -ComObject "Dism.DismManager"
+$CleanupSession = $CleanupManager.CreateSession()
+
+$CleanupSession.LogPath = "C:\Windows\Logs\DISM\dism.log"
+$CleanupSession.LogLevel = 1
+
+$CleanupProvider = $CleanupManager.CreateComponent("Cleanup-Image")
